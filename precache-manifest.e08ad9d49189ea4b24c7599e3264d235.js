b25248d855b2a07cdfb7ea4e2e568d38a422e0f0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-02 10:35:34 +0100

    Updates

---
+++ precache-manifest.e08ad9d49189ea4b24c7599e3264d235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ac7a69a70f22e30621449263192373",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "20058525714fc4570af9",
+    "url": "/website/static/css/main.55aeb043.chunk.css"
+  },
+  {
+    "revision": "3b17e45aa7d21c1bd17c",
+    "url": "/website/static/js/2.857d08ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.857d08ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20058525714fc4570af9",
+    "url": "/website/static/js/main.3fca2cd9.chunk.js"
+  },
+  {
+    "revision": "c4c24515b69882d3ca76",
+    "url": "/website/static/js/runtime-main.c539387a.js"
+  }
+]);
